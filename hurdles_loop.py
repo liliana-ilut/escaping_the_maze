ea5f4dafa125962a806eb81e5390f2fc3e34683d
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2023-01-20 16:45:20 -0600

    added hurdle3

--- hurdles_loop.py
+++ hurdles_loop.py
@@ -18,3 +18,34 @@ def jump():
 
 for i in range (6):
     jump()
+
+# hurdle 3
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+def wall_jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while not at_goal(): 
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        wall_jump()

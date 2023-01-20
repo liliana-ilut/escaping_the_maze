8978c0ac4d9e2b82c32743150c8a51fece8dbd76
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2023-01-20 17:48:01 -0600

    escaping the maze file

--- hurdles_loop.py
+++ hurdles_loop.py
@@ -49,3 +49,38 @@ while not at_goal():
         move()
     elif wall_in_front():
         wall_jump()
+
+# hurdle 4
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def wall_jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()    
+    
+while not at_goal(): 
+    if wall_in_front():
+        wall_jump()
+    else:
+        move()

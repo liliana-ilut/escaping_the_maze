187186d7b111ed69dad1a45c9b9976e1abde750a
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2023-01-24 12:51:35 -0600

    updated instructions for the challenges

--- hurdles_loop.py
+++ hurdles_loop.py
@@ -1,6 +1,8 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
-# follow the reeborg link for this challenge
+# follow the reeborg link ^^ for this challenge
+# Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+# insert the below code for solution
 def turn_right():
     turn_left()
     turn_left()
@@ -21,6 +23,14 @@ for i in range (6):
 
 # hurdle 3
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+# The position and number of hurdles changes each time this world is reloaded.
+# What you need to know
+# The functions move() and turn_left().
+# The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
+# How to use a while loop and an if statement.
+
+# insert the below code for solution
 
 def turn_right():
     turn_left()
